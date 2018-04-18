8936b5c374b9ab48af55a4f29ef4c230769e2361
Kevin Chou <chouk@rose-hulman.edu>
2018-04-17 22:03:33 -0400

    /

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 2, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  April 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Kevin Chou.  April 2018.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -12,7 +12,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the code of the  Rect  class below.
+# DONE: 2.  READ the code of the  Rect  class below.
 #
 #   Once you are confident that you understand the  Rect  class and its code,
 #   change the TO-DO for this problem to DONE.
@@ -29,15 +29,37 @@ class Rect(object):
 def run_test_problem1():
     """ Tests the   problem1   function. """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement at least 2 tests of the  problem1  function.
+    # Done: 3. Implement at least 2 tests of the  problem1  function.
     # -------------------------------------------------------------------------
     print()
     print('--------------------------------------------------')
     print('Testing the   problem1   function:')
     print('--------------------------------------------------')
+    print()
+    print('Test1:')
+    rect1 = Rect(5, 10)
+    rect2 = Rect(4, 3)
+    rect3 = Rect(100, 7)
+    rectangles1 = (rect1, rect2, rect3)
+    print('Expected:', 762)
+    print('Actual:', problem1(rectangles1))
+    print()
+    print('Test2:')
+    rect4 = Rect(4, 9)
+    rect5 = Rect(6, 2)
+    rect6 = Rect(200, 6)
+    rectangles2 = (rect4, rect5, rect6)
+    print('Expected:', 1248)
+    print('Actual:', problem1(rectangles2))
 
 
 def problem1(rectangles):
+    total = 0
+    for k in range(len(rectangles)):
+        area = rectangles[k].w*rectangles[k].h
+        total = total + area
+    return total
+
     """
     What comes in:  A sequence of  Rect  objects.
     What goes out:  Returns the sum of the areas of the given  Rect  objects.
@@ -53,7 +75,7 @@ def problem1(rectangles):
     :return: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     # -------------------------------------------------------------------------
 
 
